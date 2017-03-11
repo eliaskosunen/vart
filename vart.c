6148e36ab2da85eb940b0221bdfe06634fa48cd8
Elias Kosunen <elias.kosunen@gmail.com>
2017-03-11 14:10:43 +0200

    Change Varuna main to its mangled name

--- vart.c
+++ vart.c
@@ -2,14 +2,9 @@
 /* This file is distributed under the 3-Clause BSD License */
 /* See LICENSE for details */
 
-#define VA_MANGLE(name) __va_##name
-
-extern int VA_MANGLE(main)(void);
-
-/* extern void VA_MANGLE(set_args)(int, char**); */
+extern int _Z4mainv();
 
 int main(int argc, char** argv)
 {
-    /* VA_MANGLE(set_args)(argc, argv); */
-    return VA_MANGLE(main)();
+    return _Z4mainv();
 }
